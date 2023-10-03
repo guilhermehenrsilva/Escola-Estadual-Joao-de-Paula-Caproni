b1bc936fd91b74ebc66698a041ac24b3d219f84c
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2023-10-03 08:31:33 -0300

    Guilherme 03/10/2023 - inserindo icones / carrossel

--- Modelo de Site - Matheus/Modelosite-main/script.js
+++ Modelo de Site - Matheus/Modelosite-main/script.js
@@ -87,3 +87,16 @@ gapi.load('client', initGoogleSheetsApi);
 
 
 
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    keyboard: true,
+  });
+  
